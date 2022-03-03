44dc0ee331abf63fad8a083d71358b97fb081e39
nsweeting2 <55107062+nsweeting2@users.noreply.github.com>
2022-03-03 12:56:34 -0500

    divided inroom from player gauges

--- src/scripts/XaosInterface/InterfaceScript.lua
+++ src/scripts/XaosInterface/InterfaceScript.lua
@@ -31,8 +31,7 @@ xi = xi or {
         [[CHARACTER_NAME]], [[CHARACTER_ID]], [[HEALTH]], [[HEALTH_MAX]],
         [[MOVEMENT]], [[MOVEMENT_MAX]], [[MANA]], [[MANA_MAX]],
         [[EXPERIENCE]], [[EXPERIENCE_MAX]], [[HERO_EXP]], [[HERO_EXP_TNL]],
-        [[ASCEND_EXP]], [[ASCEND_EXP_MAX]], [[SHIELDER]], [[SHIELDER_MAX]],
-        [[PARTY_MEM]], [[IN_ROOM]],
+        [[SHIELDER]], [[SHIELDER_MAX]], [[PARTY_MEM]], [[IN_ROOM]],
         },
     stylesheet = { -- all of our stylesheets, I think we should consider loading these from a lua file... 
         shieldbar_front = [[
@@ -131,6 +130,54 @@ xi = xi or {
             border-radius: 7;
             padding: 3px;
             ]],
+        targetbar_front = [[
+            background-color: rgba(255,0,0, 0%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
+        targetbar_back = [[
+            background-color: rgba(255,0,0, 50%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
+        neutralbar_front = [[
+            background-color: rgba(204,204,0, 0%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
+        neutralbar_back = [[
+            background-color: rgba(204,204,0, 50%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
+        allybar_front = [[
+            background-color: rgba(0,255,0, 0%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
+        allybar_back = [[
+            background-color: rgba(0,255,0, 50%);
+            border-width: 1px;
+            border-color: black;
+            border-style: solid;
+            border-radius: 7;
+            padding: 3px;
+            ]],
     },
 }
 
@@ -147,17 +194,67 @@ function xi.echo(text)
 
 end
 
-local function buildGaugeArea()
+local function on_IN_ROOM()
+
+    display(msdp.IN_ROOM)
+
+end
+
+local function playerGaugesAdjustment()
+
+    if tonumber(msdp.SHIELDER_MAX) == 0 then
+        xi.shieldbar:hide()
+        xi.shieldbar:stop()
+        xi.hpbar:move(0,0)
+        xi.manabar:move("50%",0)
+        xi.hpbar:resize("50%","100%")
+        xi.manabar:resize("50%","100%")
+    else
+        xi.shieldbar:show()
+        xi.shieldbar:start()
+        xi.shieldbar:move(0,0)
+        xi.hpbar:move("5%",0)
+        xi.manabar:move("50%",0)
+        xi.shieldbar:resize("5%","100%")
+        xi.hpbar:resize("45%","100%")
+        xi.manabar:resize("50%","100%")
+    end
+    xi.movebar:move("50%",0)
+    xi.expbar:move(0,0)
+    xi.herobar:move(0,0)
+    xi.movebar:resize("50%", "100%")
+    xi.expbar:resize("50%", "100%")
+    xi.herobar:resize("50%", "100%")
+
+end
+
+local function roomGaugesAdjustment()
+
+end
+
+local function partyGaugesAdjustment()
+
+end
+
+local function buildPlayerGaugeArea()
 
     --setup adjustable container
-    xi.gaugeAdjCon =  xi.gaugeAdjCon or Adjustable.Container:new({
-        name = "gaugeAdjCon",
-        x = "0%", y = "85%",
-        width = "100%", height = "15%",
-        adjLabelstyle = "background-color:rgba(0,0,0,100%); border: 5px groove grey;",
+    xi.playerGaugeAdjCon =  xi.playerGaugeAdjCon or Adjustable.Container:new({
+        name = "playerGaugeAdjCon",
+        x = "0%", y = "90%",
+        width = "100%", height = "10%",
+        adjLabelstyle = [[
+            background-color:rgba(0,0,0,100%);
+            border: 5px groove grey;
+            ]],
         buttonstyle = [[
-            QLabel{ border-radius: 7px; background-color: rgba(140,140,140,100%);}
-            QLabel::hover{ background-color: rgba(160,160,160,50%);}
+            QLabel{
+                border-radius: 7px;
+                background-color: rgba(140,140,140,100%);
+                }
+            QLabel::hover{
+                background-color: rgba(160,160,160,50%);
+                }
             ]],
         buttonFontSize = 4,
         buttonsize = 8,
@@ -166,32 +263,28 @@ local function buildGaugeArea()
         padding = 8,
         })
 
-    xi.gaugeAdjCon:attachToBorder("bottom")
+    xi.playerGaugeAdjCon:attachToBorder("bottom")
+    xi.playerGaugeAdjCon:lockContainer("light")
 
-    --now we make a vbox with four hbox's in it
-    xi.gaugeVCon = xi.gaugeVCon or Geyser.VBox:new({
-        name = "gaugeVCon",
+    --now we make a vbox with two hbox's in it
+    xi.playerGaugeVCon = xi.playerGaugeVCon or Geyser.VBox:new({
+        name = "playerGaugeVCon",
         x = 0, y = 0,
         width = "100%", height = "100%"
-        },xi.gaugeAdjCon)
+        },xi.playerGaugeAdjCon)
 
-    xi.gaugeHCon1 = xi.gaugeHCon1 or Geyser.HBox:new({
-        name = "gaugeHCon1",
-        },xi.gaugeVCon)
+    xi.playerGaugeHCon1 = xi.playerGaugeHCon1 or Geyser.HBox:new({
+        name = "playerGaugeHCon1",
+        },xi.playerGaugeVCon)
 
-    xi.gaugeHCon2 = xi.gaugeHCon2 or Geyser.HBox:new({
-        name = "gaugeHCon2",
-        },xi.gaugeVCon)
+    xi.playerGaugeHCon2 = xi.playerGaugeHCon2 or Geyser.HBox:new({
+        name = "playerGaugeHCon2",
+        },xi.playerGaugeVCon)
 
-    xi.gaugeHCon3 = xi.gaugeHCon3 or Geyser.HBox:new({
-        name = "gaugeHCon3",
-        },xi.gaugeVCon)
+    --I used doubleclick calls when we used default gauges,
+    --SUG made it not needed, but i left the code incase.
 
-    xi.gaugeHCon4 = xi.gaugeHCon4 or Geyser.HBox:new({
-        name = "gaugeHCon4",
-        },xi.gaugeVCon)
-
-    --spawn three gauges in our third hbox, for shield, hp, and mana
+    --spawn three gauges in our first hbox, for shield, hp, and mana
     --Shielder Gauge
     xi.shieldbar = xi.shieldbar or SUG:new({
         name = "shieldbar",
@@ -199,7 +292,7 @@ local function buildGaugeArea()
         textTemplate = "<center><strong>&#x1F6E1;</strong></center>",
         currentVariable = "msdp.SHIELDER",
         maxVariable = "msdp.SHIELDER_MAX",
-        },xi.gaugeHCon3)
+        },xi.playerGaugeHCon1)
 
     xi.shieldbar.front:setStyleSheet(xi.stylesheet.shieldbar_front)
     xi.shieldbar.back:setStyleSheet(xi.stylesheet.shieldbar_back)
@@ -215,7 +308,7 @@ local function buildGaugeArea()
         textTemplate = "<center><strong>&#x2764;: |c/|m (|p%)</strong></center>",
         currentVariable = "msdp.HEALTH",
         maxVariable = "msdp.HEALTH_MAX",
-        },xi.gaugeHCon3)
+        },xi.playerGaugeHCon1)
 
     xi.hpbar.front:setStyleSheet(xi.stylesheet.hpbar_front)
     xi.hpbar.back:setStyleSheet(xi.stylesheet.hpbar_back)
@@ -229,7 +322,7 @@ local function buildGaugeArea()
         textTemplate = "<strong><center>&#9964;: |c/|m (|p%)</strong></center>",
         currentVariable = "msdp.MANA",
         maxVariable = "msdp.MANA_MAX",
-        },xi.gaugeHCon3)
+        },xi.playerGaugeHCon1)
 
     xi.manabar.front:setStyleSheet(xi.stylesheet.manabar_front)
     xi.manabar.back:setStyleSheet(xi.stylesheet.manabar_back)
@@ -244,7 +337,7 @@ local function buildGaugeArea()
         textTemplate = "<center><strong>&#129462;: |c/|m (|p%)</strong></center>",
         currentVariable = "msdp.MOVEMENT",
         maxVariable = "msdp.MOVEMENT_MAX",
-        },xi.gaugeHCon4)
+        },xi.playerGaugeHCon2)
 
     xi.movebar.front:setStyleSheet(xi.stylesheet.movebar_front)
     xi.movebar.back:setStyleSheet(xi.stylesheet.movebar_back)
@@ -258,7 +351,7 @@ local function buildGaugeArea()
         textTemplate = " ",
         currentVariable = "msdp.EXPERIENCE",
         maxVariable = "msdp.EXPERIENCE_MAX",
-        },xi.gaugeHCon4)
+        },xi.playerGaugeHCon2)
 
     xi.expbar.front:setStyleSheet(xi.stylesheet.expbar_front)
     xi.expbar.back:setStyleSheet(xi.stylesheet.expbar_back)
@@ -272,7 +365,7 @@ local function buildGaugeArea()
         textTemplate = "<center><strong>EXP: |c/|m (|p%)</center></strong>",
         currentVariable = "msdp.HERO_EXP",
         maxVariable = "msdp.HERO_EXP_TNL",
-        },xi.gaugeHCon4)
+        },xi.playerGaugeHCon2)
 
     xi.herobar.front:setStyleSheet(xi.stylesheet.herobar_front)
     xi.herobar.back:setStyleSheet(xi.stylesheet.herobar_back)
@@ -280,9 +373,150 @@ local function buildGaugeArea()
     --xi.herobar.text:setDoubleClickCallback(function() xi.gaugeDisplayToggle(5) xi.gaugeDisplayToggle(6) end)
 
     --put everyone in their place
-    xi.gaugePosAdjustment()
+    playerGaugesAdjustment()
 
-    --xi.gaugesAdjCon:hide()
+end
+
+local function buildInRoomGaugeArea()
+
+    --setup adjustable container
+    xi.inRoomGaugeAdjCon =  xi.gaugeAinRoomGaugeAdjCondjCon or Adjustable.Container:new({
+        name = "inRoomGaugeAdjCon",
+        x = "0%", y = "0%",
+        width = "100%", height = "6%",
+        adjLabelstyle = [[
+            background-color:rgba(0,0,0,100%);
+            border: 5px groove grey;
+            ]],
+        buttonstyle = [[
+            QLabel{
+                border-radius: 7px;
+                background-color: rgba(140,140,140,100%);
+                }
+            QLabel::hover{
+                background-color: rgba(160,160,160,50%);
+                }
+            ]],
+        buttonFontSize = 4,
+        buttonsize = 8,
+        titleText = "",
+        titleTxtColor = "black",
+        padding = 8,
+        })
+
+    xi.inRoomGaugeAdjCon:attachToBorder("top")
+    xi.inRoomGaugeAdjCon:lockContainer("light")
+
+    --now we make a hbox
+    xi.inRoomGaugeHCon = xi.inRoomGaugeHCon or Geyser.HBox:new({
+        name = "inRoomGaugeHCon",
+        x = 0, y = 0,
+        width = "100%", height = "100%"
+        },xi.inRoomGaugeAdjCon)
+
+    --spawn up nine gauges and one label in our hbox, for in room hp bars
+    --In Room Gauges
+
+    --Target Gauge
+    xi.targetbar = xi.targetbar or SUG:new({
+        name = "targetbar",
+        updateTime = 250,
+        textTemplate = "<center><strong>&#x2764;:|p%</center></strong>",
+        --currentVariable = "",
+        --maxVariable = "",
+        },xi.inRoomGaugeHCon)
+
+    xi.targetbar.front:setStyleSheet(xi.stylesheet.targetbar_front)
+    xi.targetbar.back:setStyleSheet(xi.stylesheet.targetbar_back)
+
+    xi.targetbar:stop()
+    --xi.targetbar:hide()
+
+    xi.irbar = xi.irbar or {}
+
+    for i = 1,8 do
+        xi.irbar[i] = xi.irbar[i] or SUG:new({
+            name = "irbar"..i,
+            updateTime = 500,
+            textTemplate = "<center><strong>&#x2764;:|p%</center></strong>",
+            --currentVariable = "",
+            --maxVariable = "",
+            },xi.inRoomGaugeHCon)
+
+        xi.irbar[i].front:setStyleSheet(xi.stylesheet.neutralbar_front)
+        xi.irbar[i].back:setStyleSheet(xi.stylesheet.neutralbar_back)
+
+        xi.irbar[i]:stop()
+        --xi.irbar[i]:hide()
+
+    end
+
+    --overflow label for when we have over 8 people in room
+    --In Room Overflow Label
+    xi.iroverflow = xi.iroverflow or Geyser.Label:new({
+        name = "iroverflow",
+        },xi.inRoomGaugeHCon)
+
+    --xi.iroverflow:echo("<left><strong>...", "18")
+    --xi.iroverflow:hide()
+
+    --put everything in its place
+    roomGaugesAdjustment()
+
+end
+
+local function buildPartyGaugeArea()
+
+    --this is not complete and needs to be fully reviewed
+
+      --spawn up nine gauges and one label in our second hbox, for ally hp bars
+    --ally Gauges
+
+    --Target Gauge
+    xi.partnerbar = xi.partnerbar or SUG:new({
+        name = "partnerbar",
+        updateTime = 250,
+        textTemplate = "<center><strong>&#x2764;:|p%</center></strong>",
+        --currentVariable = "",
+        --maxVariable = "",
+        },xi.gaugeHCon2)
+
+    xi.partnerbar.front:setStyleSheet(xi.stylesheet.allybar_front)
+    xi.partnerbar.back:setStyleSheet(xi.stylesheet.allybar_back)
+
+    xi.partnerbar:stop()
+    --xi.partnerbar:hide()
+
+    xi.allybar = xi.allybar or {}
+
+    for i = 1,8 do
+        xi.allybar[i] = xi.allybar[i] or SUG:new({
+            name = "allybar"..i,
+            updateTime = 500,
+            textTemplate = "<center><strong>&#x2764;:|p%</center></strong>",
+            --currentVariable = "",
+            --maxVariable = "",
+            },xi.gaugeHCon2)
+
+        xi.allybar[i].front:setStyleSheet(xi.stylesheet.allybar_front)
+        xi.allybar[i].back:setStyleSheet(xi.stylesheet.allybar_back)
+
+        xi.allybar[i]:stop()
+        --xi.allybar[i]:hide()
+
+    end
+
+    --overflow label for when we have over 8 people in party
+    --Ally Overflow Label
+    xi.allyoverflow = xi.allyoverflow or Geyser.Label:new({
+        name = "allyoverflow",
+        },xi.gaugeHCon2)
+
+    --xi.allyoverflow:echo("<left><strong>...", "18")
+    --xi.allyoverflow:hide()
+
+    --put everything in its place
+    partyGaugesAdjustment()
 
 end
 
@@ -320,11 +554,15 @@ local function config()
     for k, v in pairs (xi.neededMSDP) do
         sendMSDP("REPORT",tostring(v))
     end
-    
-    --setup our gauge area
-    buildGaugeArea()
 
-    --and we are done configuring QuestingWithNoax
+    --setup our gauge areas
+    buildPlayerGaugeArea()
+
+    buildInRoomGaugeArea()
+
+    --buildPartyGaugeArea()
+
+    --and we are done configuring XaosInterface
     xi.echo("The -=XAOS=- Interface has been configured.")
 
 end
@@ -389,34 +627,6 @@ function xi.downloadPackage()
 
 end
 
-function xi.gaugePosAdjustment()
-
-    if tonumber(msdp.SHIELDER) == 0 and tonumber(msdp.SHIELDER_MAX) == 0 then
-        xi.shieldbar:hide()
-        xi.shieldbar:stop()
-        xi.hpbar:move(0,0)
-        xi.manabar:move("50%",0)
-        xi.hpbar:resize("50%","100%")
-        xi.manabar:resize("50%","100%")
-    else
-        xi.shieldbar:show()
-        xi.shieldbar:start()
-        xi.shieldbar:move(0,0)
-        xi.hpbar:move("5%",0)
-        xi.manabar:move("50%",0)
-        xi.shieldbar:resize("5%","100%")
-        xi.hpbar:resize("45%","100%")
-        xi.manabar:resize("50%","100%")
-    end
-    xi.movebar:move("50%",0)
-    xi.expbar:move(0,0)
-    xi.herobar:move(0,0)
-    xi.movebar:resize("50%", "100%")
-    xi.expbar:resize("50%", "100%")
-    xi.herobar:resize("50%", "100%")
-
-end
-
 --handles our annonymus events
 function xi.eventHandler(event, ...)
 
@@ -462,4 +672,10 @@ xi.annonEvents = { --all of the events we will need to trigger on
     registerAnonymousEventHandler("sysUninstallPackage", "xi.eventHandler"),
     registerAnonymousEventHandler("sysTelnetEvent", "xi.eventHandler"),
     registerAnonymousEventHandler("sysWindowResizeEvent","xi.eventHandler")
+    }
+
+xi.namedEvents = { --all of the events we will need to trigger on
+    registerNamedEventHandler("noax","handle_SHIELDER","msdp.SHIELDER_MAX",playerGaugesAdjustment),
+    --registerNamedEventHandler("noax","handle_PARTY_MEM","msdp.PARTY_MEM",on_PARTY_MEM),
+    registerNamedEventHandler("noax","handle_IN_ROOM","msdp.IN_ROOM",on_IN_ROOM),
     }
